e2d180478cf1791640ff8f7574fe53321cf69866
Anrug <107628450+Anrug@users.noreply.github.com>
2022-06-16 19:47:40 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,91 +12,14 @@ APP_URL = f'https://angurheroku.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-item1 = types.KeyboardButton("Анонимно")
-item2 = types.KeyboardButton("Не анонимно")
-
-markup.add(item1, item2)
-
-def send_email(mes, message):
-    sender = "telegram.bot.96@mail.ru"
-    password = "Xg5kGfr4dcjCzpCLwwXv"
-
-    server = smtplib.SMTP('smtp.mail.ru', 587)
-    server.starttls()
-
-    try:
-        server.login(sender,password)
-        msg = MIMEText(message)
-        msg["Subject"] = mes
-        server.sendmail(sender, "telegram.bot.96@mail.ru", msg.as_string())
-
-        return "The message was sent successfully!"
-    except Exception as _ex:
-        return f"{_ex}\nCheck your login or password please!"
-
-
-Mes = ['Анонимный']
-
 @bot.message_handler(commands=['start'])
 def start(message):
-
-    bot.reply_to(message.chat.id, "Здравствуйте, Я - бот который принимает жалобы, пожелания и личные обращения", 
-    parse_mode='html', reply_markup=markup)
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-
-@bot.message_handler(content_types=['text'])
-def send(message):
-    if message.text == 'Анонимно':
-
-        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item3 = types.KeyboardButton('Книга жалоб')
-        item4 = types.KeyboardButton('Личное обращения')
-
-        markup1.add(item3, item4)
-
-        bot.send_message(message.chat.id, 'Вы выбрали анонимное сообщение, теперь выберите вид обращение', reply_markup=markup1)
-        Mes[0] = 'Анонимный'
-        bot.register_next_step_handler(message, jalob)
-
-    elif message.text == 'Не анонимно':
-        bot.send_message(message.chat.id, 'Вы выбрали не анонимное сообщение\n Напишите Фамилия Имя Очество')
-        bot.register_next_step_handler(message, neonim)
-
-    else:
-        bot.send_message(message.chat.id, 'Error')
-        
-
-def neonim(message):
-    Mes[0] = message.text
-    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item5 = types.KeyboardButton('Книга жалоб')
-    item6 = types.KeyboardButton('Личное обращения')
-
-    markup1.add(item5, item6)
-    bot.send_message(message.chat.id, 'Теперь выберите вид обращения', reply_markup=markup1)
-    bot.register_next_step_handler(message, jalob)
-
-
-def jalob(message):
-    if message.text == 'Книга жалоб':
-        bot.send_message(message.chat.id, 'Вы выбрали книгу жалоб\n Напишите свое сообщение!')
-        bot.register_next_step_handler(message, sendMesjal)
-    elif message.text == 'Личное обращения':
-        bot.send_message(message.chat.id, 'Вы выбрали личное обращение\n Напишите свое сообщение!')
-        bot.register_next_step_handler(message, sendMeslich)
-    
-    
-def sendMesjal(message):
-    print(send_email(mes = 'Книга жалоб', message=message.text + "\n\n\t" + "Автор: " + Mes[0]))
-    bot.send_message(message.chat.id, 'Ваше сообщение успешно отправлено!', reply_markup=markup)
-    bot.register_next_step_handler(message, start)
-
-def sendMeslich(message):
-    print(send_email(mes = 'Личное обращения', message=message.text + "\n\n\t" + "Автор: " + Mes[0]))
-    bot.send_message(message.chat.id, 'Ваше сообщение успешно отправлено!', reply_markup=markup)
-    bot.register_next_step_handler(message, start)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
